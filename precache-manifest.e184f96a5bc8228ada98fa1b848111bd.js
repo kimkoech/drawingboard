9c3a7af897869b6ec0286473db0722c1d3c22375
Billy Koech <bkkoech@college.harvard.edu>
2020-06-06 22:35:27 -0400

    Updates

---
+++ precache-manifest.e184f96a5bc8228ada98fa1b848111bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1946cba72356f44b2c4655d4942433b8",
+    "url": "/drawingboard/index.html"
+  },
+  {
+    "revision": "b7a7f92c82c0c185e11a",
+    "url": "/drawingboard/static/css/main.dbb10094.chunk.css"
+  },
+  {
+    "revision": "8de4037d7f5efb6177e7",
+    "url": "/drawingboard/static/js/2.438f12c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drawingboard/static/js/2.438f12c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a7f92c82c0c185e11a",
+    "url": "/drawingboard/static/js/main.c778121f.chunk.js"
+  },
+  {
+    "revision": "b477f69a213b6f09de88",
+    "url": "/drawingboard/static/js/runtime-main.b2577187.js"
+  },
+  {
+    "revision": "10a846697c1c9f3c59b0afbba01b30cb",
+    "url": "/drawingboard/static/media/avatar_hat.10a84669.jpg"
+  },
+  {
+    "revision": "2b1ec51959d6520d885adb0858584f4b",
+    "url": "/drawingboard/static/media/card2.2b1ec519.jpg"
+  },
+  {
+    "revision": "2b1ec51959d6520d885adb0858584f4b",
+    "url": "/drawingboard/static/media/kobu-agency-pvMVecxVoW0-unsplash.2b1ec519.jpg"
+  }
+]);
